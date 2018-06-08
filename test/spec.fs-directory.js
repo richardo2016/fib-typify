047043d0e39965518e62e03cfa997c1505e0496e
Richardo2016 <richardo2016@gmail.com>
2018-06-08 17:18:24 +0800

    support providing 'compilerOptions' when calling 'compileDirectoryTo'.

--- test/spec.fs-directory.js
+++ test/spec.fs-directory.js
@@ -5,11 +5,34 @@ const path = require('path')
 
 const fibTypify = require('../')
 
+const compilerOptions1 = {
+    target: 'es6',
+    module: 'commonjs'
+}
+const compilerOptions2 = {
+    target: 'es5',
+    module: 'commonjs'
+}
+
 describe('fs-directory', () => {
     it('compileDirectoryTo', () => {
         const baseDir = path.resolve(__dirname, './ts')
         const distDir = path.resolve(__dirname, './dist/directory')
 
         fibTypify.compileDirectoryTo(baseDir, distDir)
+    })
+
+    it('compileDirectoryTo with compilerOptions1', () => {
+        const baseDir = path.resolve(__dirname, './ts')
+        const distDir = path.resolve(__dirname, './dist/directory1')
+
+        fibTypify.compileDirectoryTo(baseDir, distDir, { compilerOptions: compilerOptions1 })
+    })
+
+    it('compileDirectoryTo with compilerOptions2', () => {
+        const baseDir = path.resolve(__dirname, './ts')
+        const distDir = path.resolve(__dirname, './dist/directory2')
+
+        fibTypify.compileDirectoryTo(baseDir, distDir, { compilerOptions: compilerOptions2 })
     })
 })
