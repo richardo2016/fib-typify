a2201b6705ef337f6d334cd42827c3e38558b28c
Ray Chan <richardo2016@gmail.com>
2020-05-24 16:50:42 +0800

    chore: clean temp dts by mistaking

--- typings/core/ts-apis/program.d.ts
+++
@@ -1,2 +0,0 @@
-export function createCompilerHost(compilerOptions: any): import("typescript").CompilerHost;
-export function createProgram(fileNames: any, compilerOptions: any, host?: import("typescript").CompilerHost): import("typescript").Program;
