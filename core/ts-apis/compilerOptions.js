2bfa7414580e94de514d5a6458ecdf28934b9f83
Ray Chan <richardo2016@gmail.com>
2020-05-25 18:58:04 +0800

    chore: code clean

--- core/ts-apis/compilerOptions.js
+++ core/ts-apis/compilerOptions.js
@@ -10,14 +10,6 @@ const compilerOptions = {
 
 exports.getDefaultCompilerOptions = () => compilerOptions
 
-exports.filterCompilerOptions = function (compilerOptions) {
-    if (compilerOptions.sourceMap) {
-        console.warn(`[fib-typify] don't support sourceMap now, tranform to 'inlineSourceMap' automatically.`)
-        compilerOptions.sourceMap = false
-        compilerOptions.inlineSourceMap = true
-    }
-}
-
 /**
  * @description return one ts.ParseConfigHost
  */
--- core/ts-apis/transpilor.js
+++ core/ts-apis/transpilor.js
@@ -5,7 +5,7 @@ const ts = require('typescript')
  * sometimes you want to compile script fragment manually rather than run the whole
  * program, you could use exported functions here.
  */
-const { filterCompilerOptions, getDefaultCompilerOptions } = require('./compilerOptions')
+const { getDefaultCompilerOptions } = require('./compilerOptions')
 
 function _getOptions(options, locals) {
     return ({
@@ -17,17 +17,12 @@ function _getOptions(options, locals) {
 
 exports.getTranspilor = function (compilerOptions, fileName, diagnostics, moduleName) {
     return (input, locals) => {
-        compilerOptions && filterCompilerOptions(compilerOptions)
-
         return ts.transpile(input, _getOptions(compilerOptions, locals), fileName, diagnostics, moduleName)
     }
 }
 
 exports.getModuleTranspilor = function (moduleOptions) {
     return (input, locals) => {
-        if (moduleOptions.compilerOptions)
-            filterCompilerOptions(moduleOptions.compilerOptions)
-
         moduleOptions.compilerOptions = _getOptions(moduleOptions.compilerOptions, locals)
 
         return ts.transpileModule(input, moduleOptions)
